efdd05f41f1d62f7943479e3ec959073f49c5d96
fran <frangarcia2707@gmail.com>
2026-04-17 13:13:23 +0200

    Segundo descanso

---
+++ LeerDatosUsuario1704/LeerDatosUsuario1704/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Cuál es tu nombre? ");
+string nombreUsuario = Console.ReadLine(); // Lee la entrada del usuario y la almacena en la variable nombreUsuario
+
+Console.WriteLine("Cuántos años tienes? ");
+int edad = int.Parse(Console.ReadLine()); // Lee la entrada del usuario, la convierte a un entero y la almacena en la variable edad
+
+Console.WriteLine($"Hola {nombreUsuario}, tienes {edad} años. Hace diez años tenías {edad - 10} años."); // Imprime un mensaje personalizado utilizando las variables nombreUsuario y edad
+
+Console.WriteLine("Cuánto dinero ganas? ");
+double salario = double.Parse(Console.ReadLine()); // Lee la entrada del usuario, la convierte a un número decimal y la almacena en la variable salario
+
+Console.WriteLine($"Tu sueldo es de {salario} dólares."); // Imprime el salario del usuario utilizando interpolación de cadenas
